c85c4df171201b367067bfa46e72a6d59dd990c1
sajad1993gh <sajad1993gh@gmail.com>
2019-07-10 19:32:40 -0400

    setting up initial files up to before orm

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require("./connection.js");
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+var orm = {
+
+}
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO burgers (burger_name)
+VALUES ("Beef Burger"), ("Chicken Burger"), ("Turkey Burger");
