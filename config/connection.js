c149ac2217c306f01583598d68676f3bd00d1f70
sajad1993gh <sajad1993gh@gmail.com>
2019-07-11 15:38:37 -0400

    adding the CLEARDB connection

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,18 @@ var mysql = require("mysql");
 
 // to protect the key from github
 require("dotenv").config();
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.KEY,
-    database: "burgers_db"
-});
-
+if (process.env.CLEARDB_DATABASE_URL) {
+    var connection = mysql.createConnection('mysql://b7ea03db4c31c9:44a7ecde@us-cdbr-iron-east-02.cleardb.net/heroku_d7538f3a08fe51e?reconnect=true');
+} else {
+    // var connection = mysql.createConnection({
+    //     host: "localhost",
+    //     port: 3306,
+    //     user: "root",
+    //     password: process.env.KEY,
+    //     database: "burgers_db"
+    // });
+    var connection = mysql.createConnection('mysql://root:' + process.env.KEY + '@localhost:3306/burgers_db');
+}
 // Make connection.
 connection.connect(function(err) {
     if (err) {
